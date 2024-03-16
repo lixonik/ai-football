4889203688364e2214e422678bc97a71714a5ae2
Boris Prokhorov <deimos.75@mail.ru>
2024-03-16 16:45:57 +0300

    small refactoring

--- src/agent.js
+++ src/agent.js
@@ -1,251 +1,252 @@
-const Msg = require('./msg')
-const { roundToHund, normalize, do180 } = require('./math_utils')
-const { FLAGS } = require('./constants')
-const Controller = require('./controller')
+const Msg = require("./msg")
+const { roundToHund, normalize, do180 } = require("./math_utils")
+const { FLAGS } = require("./constants")
+const Controller = require("./controller")
 const { isNil, isDefined } = require("./utils")
 
-const { Goalie, Substitute, Forward } = require('./Roles')
-const SoundManager = require('./soundManager')
+const { Goalie, Substitute, Forward } = require("./roles")
+const SoundManager = require("./soundManager")
 
-const Memory = require('./memory')
-const Manager = require('./StateMachine/Manager')
+const Memory = require("./memory")
+const Manager = require("./StateMachine/Manager")
 const FSM = require("./StateMachine/FSM")
-const GSM = require('./StateMachine/GSM')
+const GSM = require("./StateMachine/GSM")
 
 class Agent {
-    constructor(teamName, role) {
-        this.connected = false
-        this.team = teamName
-        this.side = 'l' // По умолчанию - левая половина поля
-        this.run = false // Игра начата
-        this.act = () => {} // Действия
+	constructor(teamName, role) {
+		this.connected = false
+		this.team = teamName
+		this.side = "l" // По умолчанию - левая половина поля
+		this.run = false // Игра начата
+		this.act = () => {
+		} // Действия
 
-        this.gamemode = 'before_kick_off'
-        this.objects = null
-        this.zeroVector = null
-        this.id = 0
+		this.gamemode = "before_kick_off"
+		this.objects = null
+		this.zeroVector = null
+		this.id = 0
 
-        this.x = null
-        this.y = null
-        this.controller = new Controller(this)
-        this.soundManager = new SoundManager(this)
-        this.memory = new Memory(this)
-        this.goalie = role === "goalie"
-        if (this.goalie)
-            this.player_state_machine = new GSM(this)
-        else
-            this.player_state_machine = new FSM(this)
-        
-        this.manager = new Manager(this)
+		this.x = null
+		this.y = null
+		this.controller = new Controller(this)
+		this.soundManager = new SoundManager(this)
+		this.memory = new Memory(this)
+		this.goalie = role === "goalie"
+		if (this.goalie)
+			this.player_state_machine = new GSM(this)
+		else
+			this.player_state_machine = new FSM(this)
 
-        this.onConnection = () => {
-        }
-    }
+		this.manager = new Manager(this)
 
-    msgGot(msg) { // Получение сообщения
-        if (!this.connected) {
-            this.connected = true
-            this.onConnection()
-        }
-        let data = msg.toString('utf8') // Приведение к строке
-        this.processMsg(data) // Разбор сообщения
-    }
+		this.onConnection = () => {
+		}
+	}
 
-    setSocket(socket) { // Настройка сокета
-        this.socket = socket
-    }
+	msgGot(msg) { // Получение сообщения
+		if (!this.connected) {
+			this.connected = true
+			this.onConnection()
+		}
+		let data = msg.toString("utf8") // Приведение к строке
+		this.processMsg(data) // Разбор сообщения
+	}
 
-    socketSend(cmd, value) { // Отправка команды
-        this.socket.sendMsg(`(${cmd} ${value})`)
-    }
+	setSocket(socket) { // Настройка сокета
+		this.socket = socket
+	}
 
-    processMsg(msg) { // Обработка сообщения
-        let data = Msg.parseMsg(msg) // Разбор сообщения
-        if (!data) throw new Error('Parse error\n' + msg)
-        // Первое (hear) - начало игры
-        if (data.cmd === 'hear') {
-            this.soundManager.scan(data.p)
-        }
-        if (data.cmd === 'init') this.initAgent(data.p)//Инициализация
-        this.analyzeEnv(data.msg, data.cmd, data.p) // Обработка
-        if (data.cmd === "see") {
-            if (isNil(this.role)) {
-                let countPlayer = this.objects.filter((p) => p.team === this.team)
-                if (countPlayer.length === 0) this.role = new Forward(this)
-                else this.role = new Substitute(this)
-            } 
-            this.sendCmd() // Отправка команды
-        }
-    }
+	socketSend(cmd, value) { // Отправка команды
+		this.socket.sendMsg(`(${cmd} ${value})`)
+	}
 
-    initAgent(p) {
-        if (p[0] === 'r') this.side = 'r' // Правая половина поля
-        if (p[0] === 'l') this.side = 'l'    // Левая половина поля
-        if (p[1]) this.id = p[1] // id игрока
-    }
+	processMsg(msg) { // Обработка сообщения
+		let data = Msg.parseMsg(msg) // Разбор сообщения
+		if (!data) throw new Error("Parse error\n" + msg)
+		// Первое (hear) - начало игры
+		if (data.cmd === "hear") {
+			this.soundManager.scan(data.p)
+		}
+		if (data.cmd === "init") this.initAgent(data.p)//Инициализация
+		this.analyzeEnv(data.msg, data.cmd, data.p) // Обработка
+		if (data.cmd === "see") {
+			if (isNil(this.role)) {
+				let countPlayer = this.objects.filter((p) => p.team === this.team)
+				if (countPlayer.length === 0) this.role = new Forward(this)
+				else this.role = new Substitute(this)
+			}
+			this.sendCmd() // Отправка команды
+		}
+	}
 
-    analyzeEnv(msg, cmd, p) { // Анализ сообщения
-        {
-            if (cmd !== 'see') return
+	initAgent(p) {
+		if (p[0] === "r") this.side = "r" // Правая половина поля
+		if (p[0] === "l") this.side = "l"    // Левая половина поля
+		if (p[1]) this.id = p[1] // id игрока
+	}
 
-            p.forEach(item => {
-                if (typeof (item) == 'object') this.parseObjInfo(item)
-            })
+	analyzeEnv(msg, cmd, p) { // Анализ сообщения
+		{
+			if (cmd !== "see") return
 
-            this.processObjs(p)
-            this.objects = p
-        }
-    }
+			p.forEach(item => {
+				if (typeof (item) == "object") this.parseObjInfo(item)
+			})
 
-    parseObjInfo(objInfo) {
+			this.processObjs(p)
+			this.objects = p
+		}
+	}
 
-        let cmd = objInfo.cmd
+	parseObjInfo(objInfo) {
 
-        let tmpFlagType = cmd.p.join('')
-        objInfo.x = FLAGS[tmpFlagType] ? FLAGS[tmpFlagType].x : null
-        objInfo.y = FLAGS[tmpFlagType] ? FLAGS[tmpFlagType].y : null
+		let cmd = objInfo.cmd
 
-        //Type detect
-        if (objInfo.x) objInfo.type = 'flag'
-        if (tmpFlagType === 'b') objInfo.type = 'ball'
-        if (cmd.p[0] === 'p') {
-            if (cmd.p.length > 1) {
-                objInfo.team = cmd.p[1].replace(/"/g, '')
-                objInfo.type = "player"
-                objInfo.ally = objInfo.team === this.team
-            }
-            if (cmd.p.length > 2) objInfo.number = parseInt(cmd.p[2])
-            objInfo.goalie = cmd.p.length > 3
-        }
+		let tmpFlagType = cmd.p.join("")
+		objInfo.x = FLAGS[tmpFlagType] ? FLAGS[tmpFlagType].x : null
+		objInfo.y = FLAGS[tmpFlagType] ? FLAGS[tmpFlagType].y : null
 
-        //Parse Info
-        let p = objInfo.p
+		//Type detect
+		if (objInfo.x) objInfo.type = "flag"
+		if (tmpFlagType === "b") objInfo.type = "ball"
+		if (cmd.p[0] === "p") {
+			if (cmd.p.length > 1) {
+				objInfo.team = cmd.p[1].replace(/"/g, "")
+				objInfo.type = "player"
+				objInfo.ally = objInfo.team === this.team
+			}
+			if (cmd.p.length > 2) objInfo.number = parseInt(cmd.p[2])
+			objInfo.goalie = cmd.p.length > 3
+		}
 
-        objInfo.direction = p.length > 1 ? p[1] : p[0]
-        objInfo.distance = p.length >= 2 ? p[0] : null
-        objInfo.distanceChange = p.length >= 4 ? p[2] : null
-        objInfo.directionChange = p.length >= 4 ? p[3] : null
-        objInfo.bodyFacingDirection = p.length >= 6 ? p[4] : null
-        objInfo.headFacingDirection = p.length >= 6 ? p[5] : null
-    }
+		//Parse Info
+		let p = objInfo.p
 
-    processObjs(objects) {
-        let flags = objects
-            .filter(obj => obj.x)
-            .sort((a, b) => a.distance - b.distance)
+		objInfo.direction = p.length > 1 ? p[1] : p[0]
+		objInfo.distance = p.length >= 2 ? p[0] : null
+		objInfo.distanceChange = p.length >= 4 ? p[2] : null
+		objInfo.directionChange = p.length >= 4 ? p[3] : null
+		objInfo.bodyFacingDirection = p.length >= 6 ? p[4] : null
+		objInfo.headFacingDirection = p.length >= 6 ? p[5] : null
+	}
 
-        let minError = null
-        let bestLocation = null
+	processObjs(objects) {
+		let flags = objects
+			.filter(obj => obj.x)
+			.sort((a, b) => a.distance - b.distance)
 
-        /**
-         * Find agent location
-         */
-        let i = 0
-        while (true) {
-            let cFlags = flags.slice(i, flags.length)
-            let location = this.localizeAgent(cFlags)
-            if (isNil(location)) break
+		let minError = null
+		let bestLocation = null
 
-            let error = 0
-            for (let flag of flags) {
-                let estimated = FLAGS.distance(location, { x: flag.x, y: flag.y })
-                error = Math.max(Math.abs(estimated - flag.distance), error)
-            }
+		/**
+		 * Find agent location
+		 */
+		let i = 0
+		while (true) {
+			let cFlags = flags.slice(i, flags.length)
+			let location = this.localizeAgent(cFlags)
+			if (isNil(location)) break
 
-            if (!minError || minError > error) {
-                minError = error
-                bestLocation = location
-            }
-            i++
-        }
-        this.x = bestLocation ? roundToHund(bestLocation.x) : null
-        this.y = bestLocation ? roundToHund(bestLocation.y) : null
+			let error = 0
+			for (let flag of flags) {
+				let estimated = FLAGS.distance(location, { x: flag.x, y: flag.y })
+				error = Math.max(Math.abs(estimated - flag.distance), error)
+			}
 
-        /**
-         * Find object location
-         */
-        if (!this.x) {
-            return null
-        }
-        minError = null
-        for (let flag of flags) {
-            let zeroVec = this.rotate(normalize(this, flag), flag.direction)
-            let error = 0
+			if (!minError || minError > error) {
+				minError = error
+				bestLocation = location
+			}
+			i++
+		}
+		this.x = bestLocation ? roundToHund(bestLocation.x) : null
+		this.y = bestLocation ? roundToHund(bestLocation.y) : null
 
-            flags.forEach(f => {
-                let fVec = normalize(this, f)
-                let estimated = Math.acos(zeroVec.x * fVec.x + zeroVec.y * fVec.y)
-                error = Math.max(Math.abs(estimated - flag.direction * Math.PI / 180), error)
-            })
+		/**
+		 * Find object location
+		 */
+		if (!this.x) {
+			return null
+		}
+		minError = null
+		for (let flag of flags) {
+			let zeroVec = this.rotate(normalize(this, flag), flag.direction)
+			let error = 0
 
-            if (!minError || minError > error) {
-                minError = error
-                this.zeroVector = zeroVec
-            }
-        }
+			flags.forEach(f => {
+				let fVec = normalize(this, f)
+				let estimated = Math.acos(zeroVec.x * fVec.x + zeroVec.y * fVec.y)
+				error = Math.max(Math.abs(estimated - flag.direction * Math.PI / 180), error)
+			})
 
-        //Set obj data
-        for (let obj of objects) {
-            if (typeof (obj) != 'object') {
-                continue
-            }
-            let vec = this.rotate(this.zeroVector, -obj.direction)
-            obj.x = roundToHund(this.x + vec.x * obj.distance)
-            obj.y = -roundToHund(this.y + vec.y * obj.distance)
-        }
-        this.y *= -1
-    }
+			if (!minError || minError > error) {
+				minError = error
+				this.zeroVector = zeroVec
+			}
+		}
 
-    rotate(v, objDir) {
-        let angle = objDir * (Math.PI / 180)
-        return {
-            x: v.x * Math.cos(angle) - v.y * Math.sin(angle),
-            y: v.x * Math.sin(angle) + v.y * Math.cos(angle),
-        }
-    }
+		//Set obj data
+		for (let obj of objects) {
+			if (typeof (obj) != "object") {
+				continue
+			}
+			let vec = this.rotate(this.zeroVector, -obj.direction)
+			obj.x = roundToHund(this.x + vec.x * obj.distance)
+			obj.y = -roundToHund(this.y + vec.y * obj.distance)
+		}
+		this.y *= -1
+	}
 
-    localizeAgent(flags) {
-        if (flags.length < 2) return null
+	rotate(v, objDir) {
+		let angle = objDir * (Math.PI / 180)
+		return {
+			x: v.x * Math.cos(angle) - v.y * Math.sin(angle),
+			y: v.x * Math.sin(angle) + v.y * Math.cos(angle)
+		}
+	}
 
-        let [f1, f2] = flags
+	localizeAgent(flags) {
+		if (flags.length < 2) return null
 
-        let d = FLAGS.distance({ x: f1.x, y: f1.y }, { x: f2.x, y: f2.y })
-        let alpha = (f1.direction - f2.direction) / 180 * Math.PI
+		let [f1, f2] = flags
 
-        if (!f1.distance || !f2.distance) return null
-        let d1 = f1.distance, d2 = f2.distance
+		let d = FLAGS.distance({ x: f1.x, y: f1.y }, { x: f2.x, y: f2.y })
+		let alpha = (f1.direction - f2.direction) / 180 * Math.PI
 
-        if (alpha < 0) {
-            [f1, f2] = [f2, f1];
-            [d1, d2] = [d2, d1]
-        }
-        let xt = f2.x - f1.x, yt = f2.y - f1.y
-        let len = Math.sqrt(xt ** 2 + yt ** 2)
-        xt = xt / len
-        yt = yt / len
-        let cos_b = Math.min(1, Math.max(-1, (d ** 2 + d1 ** 2 - d2 ** 2) / (2 * d * d1)))
-        let sin_b = Math.sqrt(Math.abs(1 - cos_b ** 2))
+		if (!f1.distance || !f2.distance) return null
+		let d1 = f1.distance, d2 = f2.distance
 
-        const location = {
-            x: f1.x + (xt * cos_b - yt * sin_b) * d1,
-            y: f1.y + (xt * sin_b + yt * cos_b) * d1,
-        }
+		if (alpha < 0) {
+			[f1, f2] = [f2, f1];
+			[d1, d2] = [d2, d1]
+		}
+		let xt = f2.x - f1.x, yt = f2.y - f1.y
+		let len = Math.sqrt(xt ** 2 + yt ** 2)
+		xt = xt / len
+		yt = yt / len
+		let cos_b = Math.min(1, Math.max(-1, (d ** 2 + d1 ** 2 - d2 ** 2) / (2 * d * d1)))
+		let sin_b = Math.sqrt(Math.abs(1 - cos_b ** 2))
 
-        return location
-    }
+		const location = {
+			x: f1.x + (xt * cos_b - yt * sin_b) * d1,
+			y: f1.y + (xt * sin_b + yt * cos_b) * d1
+		}
 
-    sendCmd() {
-        if (!this.run) { // Игра не начата
-            if (isNil(this.marker) && this.side === "r") {
-                this.controller.turn(180)
-                this.marker = true
-            }
-            return
-        }
-        this.memory.analyze()
-        this.act = this.manager.getAction()
-        this.act()
-    }
+		return location
+	}
+
+	sendCmd() {
+		if (!this.run) { // Игра не начата
+			if (isNil(this.marker) && this.side === "r") {
+				this.controller.turn(180)
+				this.marker = true
+			}
+			return
+		}
+		this.memory.analyze()
+		this.act = this.manager.getAction()
+		this.act()
+	}
 }
 
 module.exports = Agent // Экспорт игрока
--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-const { Command } = require('commander')
-const Agent = require('./agent')
-const readline = require('readline')
+const { Command } = require("commander")
+const Agent = require("./agent")
+const readline = require("readline")
 
 const program = new Command()
 
 program
-    .option('-p, --params <params...>', 'Agent params [x, y]: number[]')
-    .option('-t, --team <team>', 'Team name: string')
-    .option('-r, --role <role>', 'Player role')
-    .parse()
+	.option("-p, --params <params...>", "Agent params [x, y]: number[]")
+	.option("-t, --team <team>", "Team name: string")
+	.option("-r, --role <role>", "Player role")
+	.parse()
 
 const VERSION = 7
-const OURTEAM = 'IBAPRO'
+const OURTEAM = "IBAPRO"
 
 let teamName = program.opts().team ?? OURTEAM
 let role = program.opts().role ?? ""
 let agent = new Agent(teamName, role)
-require('./socket')(agent, teamName, VERSION, role === "goalie" ? "(goalie)" : "")
+require("./socket")(agent, teamName, VERSION, role === "goalie" ? "(goalie)" : "")
 const [x, y] = program.opts().params
 
 /**
  * callback on socket setup
  */
 agent.onConnection = () => {
-    agent.socketSend('move', `${x} ${y}`)
+	agent.socketSend("move", `${x} ${y}`)
 }
