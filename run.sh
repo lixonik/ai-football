f0959fa3629d51de00b95891d52916dc863f2791
Boris Prokhorov <b.prokhorov@yadro.com>
2024-04-01 15:47:01 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -10,17 +10,6 @@ pr=(
   "-t IBAPRO -x -35 -y 0"
   "-t IBAPRO -x -35 -y 25"
   "-t IBAPRO -r goalie -x -50 -y 0"
-  "-t MILKYWAY -x -10 -y 0"
-  "-t MILKYWAY -x -5 -y -25"
-  "-t MILKYWAY -x -5 -y 25"
-  "-t MILKYWAY -x -25 -y -3"
-  "-t MILKYWAY -x -25 -y 3"
-  "-t MILKYWAY -x -25 -y -15"
-  "-t MILKYWAY -x -25 -y 15"
-  "-t MILKYWAY -x -35 -y -25"
-  "-t MILKYWAY -x -35 -y 0"
-  "-t MILKYWAY -x -35 -y 25"
-  "-t MILKYWAY -r goalie -x -50 -y 0"
 )
 
 for i in "${!pr[@]}"; do
